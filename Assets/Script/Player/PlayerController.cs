457a18fc4f57884aacf8144a6b262338cd945128
Simla Beken <38881186+Simla4@users.noreply.github.com>
2022-02-10 20:23:38 +0300

    added: sideways control, cinemachine and monoSingleton

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -8,20 +8,17 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private float speed;
     [SerializeField] private float sidewaysMovementSensivity;
-
-    private float currentMousePos;
-    private float startMousePos;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float sidewaysLimitPos;
+ 
+    private Vector3 inputDrag;
+    private Vector3 prevMousePos;
 
     // Update is called once per frame
     void Update()
     {
         PlayerMovement();
+        GetInput();
+        MoveSideways();
     }
 
     private void PlayerMovement()
@@ -31,6 +28,30 @@ public class PlayerController : MonoBehaviour
 
     private void MoveSideways()
     {
-        var currentPlayerPos = playerRoot.position;
+        var currentPlayerPos = playerRoot.localPosition;
+       
+        var dragPos = Vector3.right * inputDrag.x * sidewaysMovementSensivity ;
+        if (currentPlayerPos.x + dragPos.x < sidewaysLimitPos && currentPlayerPos.x + dragPos.x > -sidewaysLimitPos)
+        {
+            playerRoot.localPosition += dragPos;
+        }
+    }
+
+    private void GetInput()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            prevMousePos = Input.mousePosition;
+        }
+        else if(Input.GetMouseButton(0))
+        {
+            var distance = Input.mousePosition - prevMousePos;
+            inputDrag = distance;
+            prevMousePos = Input.mousePosition;
+        }
+        else
+        {
+            inputDrag = Vector2.zero;
+        }
     }
 }
