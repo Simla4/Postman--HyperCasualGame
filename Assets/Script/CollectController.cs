672e4ae072b6534437f6ed0b0ee5924270bde493
Simla Beken <38881186+Simla4@users.noreply.github.com>
2022-02-18 00:35:21 +0300

    Added: Lerp Mechanics

--- Assets/Script/CollectController.cs
+++ Assets/Script/CollectController.cs
@@ -7,6 +7,19 @@ public class CollectController : MonoSingleton<CollectController>
 {
 
     public List<GameObject> collectable = new List<GameObject>();
+    public float movementDelay;
+
+    private void Update()
+    {
+        if(Input.GetButton("Fire1"))
+        {
+            MoveListElements();
+        }
+        if(Input.GetButtonDown("Fire1"))
+        {
+            MoveOrigin();
+        }
+    }
 
     public void StackObject(GameObject other, int index)
     {
@@ -14,24 +27,48 @@ public class CollectController : MonoSingleton<CollectController>
         Vector3 newPos = collectable[index].transform.localPosition;
         newPos.z += 1;
         other.transform.localPosition = newPos;
+
+        collectable.Add(other);
+
         StartCoroutine(MakeObjectBigger());
     }
 
     private IEnumerator MakeObjectBigger()
     {
 
-        for(int i = collectable.Count; i > 0; i++ )
+        for(int i = collectable.Count - 1; i > 1; i-- )
         {
+            var index = i;
+
             Vector3 scale = new Vector3(1, 1, 1);
             scale *= 1.5f;
 
-            collectable[i].transform.DOScale(scale, 0.1f).OnComplete(() =>
-            collectable[i].transform.DOScale(new Vector3(1, 1, 1), 0.1f ));
+            collectable[index].transform.DOScale(scale, 0.1f).OnComplete(() =>
+            collectable[index].transform.DOScale(new Vector3(1, 1, 1), 0.1f ));
 
             yield return new WaitForSeconds(0.05f);
         }
+    }
 
-        
+    private void MoveListElements()
+    {
+        for(int i = 1; i < collectable.Count; i++)
+        {
+            var pos = collectable[i].transform.localPosition;
+            pos.x = collectable[i - 1].transform.localPosition.x;
+            collectable[i].transform.DOLocalMove(pos, movementDelay);
+        }
+    }
+
+    private void MoveOrigin()
+    {
+
+        for (int i = 1; i < collectable.Count; i++)
+        {
+            var pos = collectable[i].transform.localPosition;
+            pos.x = collectable[0].transform.localPosition.x;
+            collectable[i].transform.DOLocalMove(pos, 0.7f);
+        }
     }
     
 }
