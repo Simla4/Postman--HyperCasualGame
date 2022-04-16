861d289ba62f0664e8ad9bff2acbee17f2ecd488
Olcay ÇİFTÇİ <olcaycft95@gmail.com>
2022-04-16 14:57:34 +0300

    Add: particle with obstacle collision

--- Assets/Script/Collectables/CollisionObstacle.cs
+++ Assets/Script/Collectables/CollisionObstacle.cs
@@ -1,15 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CollisionObstacle : MonoBehaviour
 {
+    [SerializeField] private GameObject smokeVFX;
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Obstacle"))
         {
+            var temporaryVFX = Instantiate(smokeVFX, transform.position, Quaternion.identity);
+            temporaryVFX.transform.SetParent(null);
+            temporaryVFX.transform.localScale *= 1.5f;
+            var temporaryVFXPos=temporaryVFX.transform.position;
+            temporaryVFXPos.y += 1f;
+            temporaryVFXPos.z -= 0.5f;
+            temporaryVFX.transform.position = temporaryVFXPos;
             Destroy(gameObject);
-
+            Destroy(temporaryVFX,0.5f);
             CollectController.Instance.collectable.Remove(gameObject);
         }
     }
